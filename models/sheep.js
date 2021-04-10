a0862503cc1c30bc9f21c88a315bf1fbc4b2278f
Alexander <lucky-pa777@yandex.ru>
2021-04-10 19:12:42 +0300

    Схема для овец

--- models/sheep.js
+++ models/sheep.js
@@ -2,8 +2,56 @@ const mongoose = require("mongoose")
 const Schema = mongoose.Schema
 
 const SheepSchema = new Schema({
-    _id: mongoose.Schema.Types._ObjectId,
+    passport: {
+        // Информация о рождении
+        chipNo: { // Номер чипа
+            type: String,
+            required: true
+        },
+        breed: { // Порода
+            type: String,
+            required: true
+        },
 
+        birthday: { // День рождения
+            type: Schema.Types.Date,
+            required: true,
+        },
+        dateOfEntry : { // Дата поступления
+            type: Schema.Types.Date,
+            required: true,
+        },
+
+        // Информация о содержании
+        farm: { // Принадлежность к ферме
+            type: Schema.Types.ObjectId,
+            ref: "farm",
+            required: true
+        },
+        chaban: { // Чабан
+            type: Schema.Types.ObjectId,
+            ref: "chaban",
+        },
+
+        // Масть
+        colorPrimary: { // Окраска шерстяного покрова
+            type: String,
+        },
+        colorSecondary: { // Окраска кроющего волоса
+            type: String,
+        },
+        colorSecondaryOpt: { // Доп. окраска кроющего волоса
+            type: String,
+        },
+
+        // Выбытие
+        dateOfDisposal:{
+            type: Date
+        },
+        reasonOfDisposal:{
+            type: String
+        }
+    }
 });
 
 module.exports = mongoose.model("sheep", SheepSchema)
